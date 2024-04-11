a0c56d43847d9a93a534696eae7df987c0a61eca
Thapelo <thapeloletsapa@gmail.com>
2024-04-11 11:24:15 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,34 @@
 // This one will be a little tricky. So check out this overview first: https://www.youtube.com/watch?v=sJ-c3BA-Ypo
 
 // 1. Create a variable to store the singleton instance of the bank branch. "bankBranchInstance"
-
+let bankBranchInstance ;
 // 2. Define a class called `BankBranch` for managing branch information.
+class BankBranch {
+    constructor(branchInfo){
+      if (!BankBranch.Instance){
+    this.branchInfo = branchInfo;
+    bankBranchInstance = this ;
+      }
+      return BankBranch.Instance;
+    }
 
-// 3. In the `BankBranch` class:
-//    - Create a constructor that takes `branchInfo` as a parameter.
-//    - Inside the constructor, check if the `bankBranchInstance` variable is null (indicating no instance exists).
-//    - If `bankBranchInstance` is null, create a new instance with the provided `branchInfo` and assign it to `bankBranchInstance`.
-//    - Return the `bankBranchInstance` whether it's newly created or existing.
 
-// 4. Add methods to the `BankBranch` class for managing branch-related information. For example, you can add a method like `getBranchInfo` to retrieve branch details.
+    getBranchInfo() {
+        return this.branchInfo;
+      }
+}
 
-// 5. In the usage section:
-//    - Create instances of the `BankBranch` class, such as `branchA` and `branchB`, with different branch information.
-//    - Use the `getBranchInfo` method to retrieve branch information from the instances.
-//    - Verify that `branchA` and `branchB` are both referring to the same instance by comparing them using `===`.
 
-// This pseudo-code provides a step-by-step explanation of how to implement the Singleton Pattern for managing a single instance of the `BankBranch` class throughout the application, ensuring that multiple instances refer to the same object.
+// Usage Section 
+
+const branchA = new BankBranch({name:'Branch A' , location:" Mandela Road Branch"  , manager: "Thapelo Letsapa"});
+
+const branchB = new BankBranch({name:'Branch B', location:"Houghton", manager:"Palesa"});
+
+
+console.log(branchA.getBranchInfo());
+console.log(branchB.getBranchInfo());
+console.log(branchA===branchB);
 
 // NOTES:
 // This example demonstrates the Singleton pattern by creating a single instance of the `BankBranch` class.
